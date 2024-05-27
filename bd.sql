94504dacd98084a52ceb2a8726ad1d5375384a3c
OlivBryan <167815327+OlivBryan@users.noreply.github.com>
2024-05-27 18:03:53 -0300

    Add files via upload

--- bd.sql
+++ bd.sql
@@ -2,9 +2,12 @@ create database bob;
 use bob;
 
 create table usuario(
-    id int auto_incriment not null,
+    id int auto_increment not null,
     login varchar(120) null,
     senha varchar(120) null,
     email varchar(120) null,
     primary key(id)
-)
+);
+
+use bob;
+select * from usuario;
---
+++ teste.js
@@ -0,0 +1,3 @@
+const app = require('./app')
+
+app.inserir('guarana', 'cocalight', 'guarana@gmail.com')
